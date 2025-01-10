9ccf0013907d5c1add3bf7797831f6446227aba2
qqweweqewe <golubkovivan4@gmail.com>
2025-01-10 15:35:43 +0300

    first commit, first program

---
+++ hello-world/src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    println!("Hi there! What's your name?");
+
+    let mut name: String = String::new();
+    io::stdin().read_line(&mut name).expect("something went wrong");
+
+    println!("Hello, {}!", name.trim());
+}
