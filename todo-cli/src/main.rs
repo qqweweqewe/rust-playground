8d7897809bd3f3a2f2fe7560d2e2ba6334b466ac
qqweweqewe <golubkovivan4@gmail.com>
2025-01-31 17:04:37 +0300

    ToDo CLI minor overhaul

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,29 +1,32 @@
 use clap::{Parser, Subcommand};
-use std::path::PathBuf;
-use std::fs::{OpenOptions, write, read_to_string, create_dir_all};
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
+use std::fs::{create_dir_all, read_to_string, write, OpenOptions};
 use std::io::Error;
+use std::path::PathBuf;
 
 #[derive(Subcommand)]
 enum Keyword {
     #[command(about = "add a new task to your tasks")]
     Add {
         #[arg(help = "a brief description of a task")]
-        description: String,  // positional argument after `add`
+        description: String, // positional argument after `add`
     },
-    
+
     #[command(about = "remove a task by its ID")]
-    Remove {
-        #[arg(help = "ID of a task to remove, check using 'list command'")]
-        id: u32,  // positional argument after `remove` and so on
+    Rm {
+        #[arg(help = "ID of a task to remove, check using 'list' command")]
+        id: u32, // positional argument after `remove` and so on
     },
-    
+
     #[command(about = "mark a task as complete")]
-    Complete {
+    Done {
         #[arg(help = "ID of a task to mark as done, check using 'list command'")]
         id: u32,
     },
     
+    #[command(about = "clear all tasks, delete all task data")]
+    Drop,
+
     #[command(about = "list all the tasks you have (useful to check IDs)")]
     List,
 }
@@ -43,9 +46,9 @@ struct ToDo {
 
 #[derive(Serialize, Deserialize)]
 struct Task {
-    id: i32,
+    id: usize,
     description: String,
-    done: bool
+    done: bool,
 }
 
 impl Task {
@@ -55,35 +58,57 @@ impl Task {
 }
 
 struct ToDoList {
-    tasks: Vec<Task>
+    tasks: Vec<Task>,
 }
 
 impl ToDoList {
     pub fn add_task(&mut self, desc: String) {
         let new_task = Task {
-            id: self.tasks.len() as i32,
+            id: self.tasks.len(),
             description: desc,
-            done: false
+            done: false,
         };
 
-        self.tasks.push(new_task) 
+        self.tasks.push(new_task)
     }
 
-    pub fn remove_task(&mut self, id: u32) {
-        self.tasks.remove(id as usize);
+    pub fn remove_task(&mut self, id: u32) -> Result<(), String> {
+        let idx = id as usize;
+
+        if idx >= self.tasks.len() {
+            return Err(format!("Invalid ID: {}", id));
+        }
+
+        self.tasks.remove(idx);
+
+        for (new_id, task) in self.tasks.iter_mut().skip(idx).enumerate() {
+            task.id = idx + new_id;
+        }
+
+        Ok(())
     }
 
-    pub fn complete_task(&mut self, id: u32) {
+    pub fn drop(&mut self) {
+        self.tasks.clear();
+    }
+
+    pub fn complete_task(&mut self, id: u32) -> Result<(), String> {
         let task_instance: Option<&mut Task> = self.tasks.get_mut(id as usize);
         match task_instance {
-            Some(instance) => { instance.mark_unmark() },
-            None => { panic!("No task with that id") }
+            Some(instance) => instance.mark_unmark(),
+            None => return Err("No task with that id".to_string()),
         };
+        Ok(())
     }
 
     pub fn list_tasks(&self) {
         for instance in &self.tasks {
-           println!("{}. {} [{}]", instance.id, instance.description, if instance.done { "x" } else { " " }); 
+            println!(
+                "{}. {} [{}]",
+                instance.id,
+                instance.description,
+                if instance.done { "x" } else { " " }
+            );
         }
     }
 
@@ -94,14 +119,14 @@ impl ToDoList {
 
     pub fn load(path: &PathBuf) -> Result<Self, Error> {
         let data = read_to_string(path)?;
-        let tasks = serde_json::from_str(&data)?; 
+        let tasks = serde_json::from_str(&data)?;
         Ok(Self { tasks })
     }
-
 }
 
-fn main() {
-    let data_path: PathBuf = setup_config_dir().expect("something went wrong while loading data :(");
+fn main() -> Result<(), String> {
+    let data_path: PathBuf =
+        setup_config_dir().expect("something went wrong while loading data :(");
 
     let mut list = ToDoList::load(&data_path).expect("failed to load data");
 
@@ -109,49 +134,51 @@ fn main() {
 
     match args.command {
         Some(Keyword::Add { description }) => {
-            println!("Added: {}", description);
-            list.add_task( description);
+            println!("Adding: {}", description);
+            list.add_task(description);
         }
-        
-        Some(Keyword::Remove { id }) => {
-            println!("Removed ID: {}", id);
-            list.remove_task(id);
+
+        Some(Keyword::Rm { id }) => {
+            println!("Removing ID: {}", id);
+            list.remove_task(id)?;
         }
-        
+
         Some(Keyword::List) => {
             list.list_tasks();
         }
-        
-        Some(Keyword::Complete { id }) => {
+
+        Some(Keyword::Done { id }) => {
             println!("Task {} marked as done", id);
-            list.complete_task(id);
+            list.complete_task(id)?;
         }
-        
+
+        Some(Keyword::Drop) => {
+            list.drop();
+        }
+
         None => {
             println!("Type 'todo-cli help' to see available commands")
         }
     }
 
     list.save(&data_path);
+    Ok(())
 }
 
 fn setup_config_dir() -> Result<PathBuf, Error> {
     let data_path: PathBuf = dirs::home_dir()
-    .expect("Home directory not found")
-    .join(".local/share")
-    .join(env!("CARGO_PKG_NAME"));
+        .expect("Home directory not found")
+        .join(".local/share")
+        .join(env!("CARGO_PKG_NAME"));
 
     create_dir_all(&data_path)?;
-    
+
     let file: PathBuf = data_path.join("data.json");
 
     if !file.exists() {
         write(&file, "[]")?;
     } else {
-        OpenOptions::new()
-            .read(true)
-            .append(true)
-            .open(&file)?;
+        OpenOptions::new().read(true).append(true).open(&file)?;
     }
 
     Ok(file)
